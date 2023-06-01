9c3c89ec32c95c3361c547a3050245c4bee19933
fitodac <fitodac@gmail.com>
2023-06-01 06:47:12 -0300

    layout

---
+++ layouts/default.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { useRoute, useRouter } from '@nuxtjs/composition-api'
+import 'remixicon/fonts/remixicon.css'
+import PorqueElegirnos from '@/components/sections/PorqueElegirnos'
+
+const route = useRoute()
+const router = useRouter()
+</script>
+
+
+<template>
+	<div class="main-wrapper">
+		<topbar/>
+		<PageHeader/>
+		<nuxt/>
+
+		<div v-if="'por-que-elegirnos' !== route.name && 'contacto' !== route.name">
+			<PorqueElegirnos/>
+			<div class="bg-gray-100 h-20"></div>
+		</div>
+
+		<PageFooter/>
+	</div>
+</template>
---
+++ layouts/homepage.vue
@@ -0,0 +1,13 @@
+<script setup>
+import 'remixicon/fonts/remixicon.css'
+</script>
+
+
+<template>
+	<div class="main-wrapper">
+		<topbar/>
+		<PageHeader/>
+		<nuxt/>
+		<PageFooter/>
+	</div>
+</template>
